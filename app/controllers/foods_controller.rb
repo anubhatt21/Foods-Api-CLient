680b294671bba1a8a087b4ca7eef7168c677d30e
Anuradha Bhatt <anudave21@gmail.com>
2017-04-06 20:23:37 -0500

    Create, Update and Delete Requests

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,47 @@
+class FoodsController < ApplicationController
+
+  def index
+    @foods = Unirest.get("http://localhost:3000/api/v1/foods").body
+    render "index.html.erb"
+  end
+
+  def show
+    @food = Unirest.get("http://localhost:3000/api/v1/foods", parameters: {id: params["id"]}).body
+    render "show.html.erb"
+  end
+
+  def new
+    render "new.html.erb" 
+  end
+
+  def create
+    @food = Unirest.post("http://localhost:3000/api/v1/foods", 
+      parameters: {
+      name: params["form_name"],
+      origin: params["form_origin"]
+      }
+      ).body
+    redirect_to "/foods/#{@food["id"]}"
+  end
+
+  def edit 
+    @food = Unirest.get("http://localhost:3000/api/v1/foods/#{params[:id]}").body
+    render "edit.html.erb"
+  end
+
+  def update
+    @food = Unirest.patch("http://localhost:3000/api/v1/foods/#{params[:id]}",
+      parameters: {
+      name: params["form_name"],
+      origin: params["form_origin"]
+      }
+      ).body
+    redirect_to "/foods/#{@food["id"]}"
+  end
+
+  def destroy
+    @food = Unirest.delete("http://localhost:3000/api/v1/foods/#{params[:id]}").body
+    render "destroy.html.erb"
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get "/foods" => "foods#index"
+  get "/foods/new" => "foods#new"
+  post "/foods" => "foods#create"
+  get "/foods/:id" => "foods#show"
+  get "/foods/:id/edit" => "foods#edit"
+  patch "/foods/:id" => "foods#update"
+  delete "/foods/:id" => "foods#destroy"
+
+  
+end
